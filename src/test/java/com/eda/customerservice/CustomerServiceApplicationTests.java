bd6a7de3afa7d9086e3d40305d963465c2ceef8a
asankaar <asanka_mdr_arachchige@manulife.com>
2022-01-06 18:30:21 +0800

    Initial spring boot application

---
+++ src/test/java/com/eda/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eda.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
